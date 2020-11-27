a4a3546d00a58a9d43f704b416e9e41e7945effb
Juan Diego Garcia <juandigv69@gmail.com>
2020-11-26 20:12:05 -0400

    basic automation on pose and img generation, Video playback controlled by button

--- openpose/OpenPosePlugin/Assets/OpenPose/Examples/Scripts/Timer.cs
+++ openpose/OpenPosePlugin/Assets/OpenPose/Examples/Scripts/Timer.cs
@@ -2,26 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class Timer : MonoBehaviour
 {
     public float timeRemaining = 10;
     public bool timerIsRunning = false;
     public Text timeText;
+    public VideoPlayer videoPlay;
 
     public void ToggleTimerStart()
     {
         timeRemaining = 10;
         Start();
-      
+        timerIsRunning = true;
+        videoPlay.Play();
     }
 
     private void Start()
     {
         Debug.Log("Lets get it started");
-        // Starts the timer automatically
-        timerIsRunning = true;
         timeText.text = "00:00";
+        timeRemaining = (float) videoPlay.clip.length;
     }
 
     void Update()
